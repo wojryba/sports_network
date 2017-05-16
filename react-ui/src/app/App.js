70ffbb936b950c5037fd6b2338db90250205cab6
JimRhead <j.m.rhead@gmail.com>
2017-05-16 15:55:58 +0100

    add homepage

--- react-ui/src/app/App.js
+++ react-ui/src/app/App.js
@@ -1,19 +1,13 @@
 /* eslint linebreak-style: 0 */
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Home from './home/Home';
+import 'normalize.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Home />
       </div>
     );
   }
---
+++ react-ui/src/app/home/Home.js
@@ -0,0 +1,48 @@
+/* eslint linebreak-style: 0 */
+import React, { Component } from 'react';
+import './Home.css';
+import homeImage from './assets/pexels-photo-114296-edited.jpg'
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <div className="HeaderBar">
+        <h1 className="Header Header_Bar">
+          Sports<br/> Network
+        </h1>
+        <h1 className="Header_About Header_Bar">
+          About
+        </h1>
+        <h1 className=" Header_Login_Border Header_Login Header_Bar">
+          Login
+        </h1>
+      </div>
+      <div className="ImageMiddle">
+        <img className="Middle_Image" alt="soccer field" src={homeImage} />
+        <h1 className="Middle_Text">Get Active. <br/> Get Involved.</h1>
+        <div className="Middle_Button">Get Started.</div>
+      </div>
+      <div className="BottomBanner">
+        <div className="Bottom_Center">
+          <ul className="Bottom_Layout Bottom_Using_Sports_Network_Column">
+            <li className="Bottom_Bold">Using Sports Network</li>
+            <li>Privacy</li>
+            <li>Terms Of Service</li>
+            <li>Local Groups</li>
+            <li>Guide To Running Events</li>
+          </ul>
+          <ul className="Bottom_Layout Bottom_About_Us_Column">
+            <li className="Bottom_Bold">About Us</li>
+            <li>Contact Us</li>
+            <li>Careers</li>
+            <li>Tools</li>
+          </ul>
+      </div>
+      </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
